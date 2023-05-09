38829cad2d291b220f8081b13ca27f4b103cb3ee
labibaadinda <100503649+labibaadinda@users.noreply.github.com>
2023-05-09 19:49:05 +0700

    BujurSaangkar.java

---
+++ Prak 5 /BujurSaangkar.java
@@ -0,0 +1,14 @@
+/*
+	Nama  File	: BujurSangkar.java
+	Pembuat	  	: Labiba Adinda Zahwana
+	NIM		    	: 24060121140111
+	Tanggal		  : Rabu, 9 Mei 2023
+	Deskripsi	  : Bujur Sangkar
+*/
+ 
+ public class BujurSangkar extends BangunDatar{
+ public double hitungLuas(double sisi){
+		luas = sisi*sisi;
+		return luas;
+	}
+ }
