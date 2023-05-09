2764c044e27c01e28ea25757e3894a5a5d89adf7
labibaadinda <100503649+labibaadinda@users.noreply.github.com>
2023-05-09 19:45:51 +0700

    BangunDatar.java

---
+++ Prak 5 /BangunDatar.java
@@ -0,0 +1,23 @@
+/*
+	Nama  File	: BangunDatar.java
+	Pembuat		: Labiba Adinda Zahwana
+	NIM			: 24060121140111
+	Tanggal		: Rabu, 9 Mei 2023
+	Deskripsi	: Abstrak Bangun Datar
+*/
+ 
+ public abstract class BangunDatar{
+	protected double luas;
+	
+	public abstract double hitungLuas(double sisi);
+	
+	public void setLuas(double l){
+		luas = l;
+	}
+	
+	public double getLuas(){
+		return luas;
+	}
+ } 
+ 
+ 
