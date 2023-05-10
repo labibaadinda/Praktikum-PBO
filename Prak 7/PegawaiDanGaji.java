a21d5b947e59f25f0395d97e76b039dcbb294280
labibaadinda <100503649+labibaadinda@users.noreply.github.com>
2023-05-10 22:24:34 +0700

    Prak 7

---
+++ Prak 7/PegawaiDanGaji.java
@@ -0,0 +1,66 @@
+/*
+	Nama  File	: PegawaiDanGaji.java
+	Pembuat		: Labiba Adinda Zahwana
+	NIM			: 24060121140111
+	Tanggal		: Rabu, 10 Mei 2023
+	Deskripsi	: File yang berisi superclass Pegawai, dua subclassnya yaitu Manager dan Programmer, sebuah class Payroll, dan file Main-nya.
+*/
+
+class Pegawai {
+	private String nama;
+	private int gajiPokok = 5000000;
+	
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	
+	public void tampilData() {
+		System.out.printf("Nama : %s, Gaji Pokok : %d \n",
+				this.nama,this.gajiPokok);
+	}
+}
+
+class Manajer extends Pegawai{
+	private int tunjangan = 700000;
+	
+	public Manajer(String nama) {
+		this.setNama(nama);
+	}
+	
+	public void tampilData() {
+		super.tampilData();
+		System.out.printf("Tunjangan : %d \n", this.tunjangan);
+	}
+}
+
+class Programmer extends Pegawai{
+	private int bonus = 450000;
+	
+	public Programmer(String nama) {
+		this.setNama(nama);
+	}
+	
+	public void tampilData() {
+		super.tampilData();
+		System.out.printf("Bonus : %d \n", this.bonus);
+	}
+}
+
+class Payroll{
+	public void cetakGaji(Pegawai pegawai) {
+		pegawai.tampilData();
+	}
+}
+
+public class PegawaiDanGaji{
+	public static void main(String args[]) {
+		Pegawai pegawai = new Programmer("Mira");
+		Pegawai pegawai2 = new Manajer("Joko");
+		Pegawai pegawai3 = new Manajer("Argo");
+		
+		Payroll payroll = new Payroll();
+		payroll.cetakGaji(pegawai);
+		payroll.cetakGaji(pegawai2);
+		payroll.cetakGaji(pegawai3);
+	}
+}
