4364c505f6efbf807671e506ff8e9f4722e01dc1
labibaadinda <100503649+labibaadinda@users.noreply.github.com>
2023-05-10 08:35:28 +0700

    Prak 5

--- Prak 5 /Lingkaran.java
+++ Prak 5 /Lingkaran.java
@@ -1,9 +1,9 @@
 /*
 	Nama  File	: Lingkaran.java
-	Pembuat		  : Labiba Adinda Zahwana
-	NIM			    : 24060121140111
-	Tanggal		  : selasa, 09 mei 2023
-	Deskripsi	  : Lingkaran
+	Pembuat	        : Labiba Adinda Zahwana
+	NIM    	        : 24060121140111
+	Tanggal		: selasa, 09 mei 2023
+	Deskripsi	: Lingkaran
 */
 
  import static java.lang.Math.PI;
