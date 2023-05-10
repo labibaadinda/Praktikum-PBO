ecbf545384338d241c076ccbff8d2358d0d0ea70
labibaadinda <100503649+labibaadinda@users.noreply.github.com>
2023-05-10 08:34:46 +0700

    Prak 5

--- Prak 5 /IArea.java
+++ Prak 5 /IArea.java
@@ -1,9 +1,9 @@
 /*
 	Nama  File	: IArea.java
 	Pembuat	  	: Labiba Adinda Zahwana
-	NIM		    	: 24060121140111
+	NIM		: 24060121140111
 	Tanggal	  	: Selasa, 9 Mei 2023
-	Deskripsi	  : Interface untuk nanti diimplementasikan oleh bangun datar untuk menghitung luasnya masing-masing
+	Deskripsi       : Interface untuk nanti diimplementasikan oleh bangun datar untuk menghitung luasnya masing-masing
 */
  
  public interface IArea{
