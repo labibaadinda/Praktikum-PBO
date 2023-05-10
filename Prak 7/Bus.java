3e7c048854b30ca37212740aac2e313476766e4e
labibaadinda <100503649+labibaadinda@users.noreply.github.com>
2023-05-10 22:22:17 +0700

    Prak 7

---
+++ Prak 7/Bus.java
@@ -0,0 +1,10 @@
+/*
+	Nama  File	: Bus.java
+	Pembuat		: Labiba Adinda Zahwana
+	NIM			: 24060121140111
+	Tanggal		: Rabu, 10 Mei 2023
+	Deskripsi	: Subclass dari Vehicle yang digunakan untuk menghitung biaya sewa bis
+*/
+
+public class Bus extends Vehicle{
+}
