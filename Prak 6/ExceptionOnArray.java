25100544a9126168d3c618546c098155f4c0dd93
labibaadinda <100503649+labibaadinda@users.noreply.github.com>
2023-05-10 08:55:57 +0700

    Prak 6

---
+++ Prak 6/ExceptionOnArray.java
@@ -0,0 +1,22 @@
+/*
+	Nama  File	: ExceptionOnArray.java
+	Pembuat		  : Labiba Adinda Zahwana
+	NIM			    : 24060121140111
+	Tanggal	   	: Rabu, 09 Mei 2023
+	Deskripsi	  : Program penggunaan eksepsi menggunakan class library pada Java
+*/
+ 
+public class ExceptionOnArray{
+	public static void main(String[] args){
+		//instansiasi object array integer
+		Integer[] arrayInteger = new Integer[4];
+		try{
+			arrayInteger[2] = 11;
+			arrayInteger[4] = 10;
+		}catch(ArrayIndexOutOfBoundsException exception){
+			exception.printStackTrace();
+		}finally{
+			System.out.println("Clean up code...");
+		}
+	}
+}
