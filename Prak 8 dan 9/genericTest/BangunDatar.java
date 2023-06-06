b0b8fc0d81dc22c28c1050674a59efc24c85e517
labibaadinda <100503649+labibaadinda@users.noreply.github.com>
2023-06-06 12:24:49 +0700

    Prak 8 dan 9

---
+++ Prak 8 dan 9/genericTest/BangunDatar.java
@@ -0,0 +1,9 @@
+/*
+	Nama  File	: BangunDatar.java
+	Pembuat		: Labiba Adinda Zahwana
+	NIM			: 24060121140111
+	Deskripsi	: kelas abstract untuk bangun datar
+*/
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+}
